86b56502d08651caf89f210d7d3a6d06d72b4b0f
cabiro2 <vitor.serrao@soulasalle.com.br>
2025-06-12 19:37:29 -0300

    Update Paciente.java

--- g2 td3/src/model/Paciente.java
+++ g2 td3/src/model/Paciente.java
@@ -1,1 +1,36 @@
+package model;
+
+public class Paciente extends Pessoa {
+    private String convenio;
+    private String numeroCartao;
+    
+    public Paciente(String nome, String cpf, String telefone, String email, String convenio, String numeroCartao) {
+        super(nome, cpf, telefone, email);
+        this.convenio = convenio;
+        this.numeroCartao = numeroCartao;
+    }
+    
+    public String getConvenio() {
+        return convenio;
+    }
+    
+    public void setConvenio(String convenio) {
+        this.convenio = convenio;
+    }
+    
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+    
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+    
+    @Override
+    public void exibirDados() {
+        System.out.println("PACIENTE - Nome: " + nome + " | CPF: " + cpf + 
+                          " | Telefone: " + telefone + " | Email: " + email +
+                          " | Convênio: " + convenio + " | Cartão: " + numeroCartao);
+    }
+}
 
